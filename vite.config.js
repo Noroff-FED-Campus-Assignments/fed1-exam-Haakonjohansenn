825037c8a1add532d6b1751bbf226d0dd66ceea2
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-05-23 17:34:39 +0200

    changed vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 export default defineConfig({
   build: {
-    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
