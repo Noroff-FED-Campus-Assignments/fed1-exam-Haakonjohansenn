baaae93824170093de5091ee9450af78825e7da8
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-05-23 17:36:48 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+         main: resolve(__dirname, "index.html"),
          contact: resolve(__dirname, "contact.html"),
          blog: resolve(__dirname, "blog.html"),
          detail: resolve(__dirname, "details.html"),
